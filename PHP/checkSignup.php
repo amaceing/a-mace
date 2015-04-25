650abb508ff6c01222b452ac752fca11ce488687
Anthony Mace <amace93@gmail.com>
2015-04-24 23:33:48 -0700

    implemented singing up and being put in database

--- PHP/checkSignup.php
+++ PHP/checkSignup.php
@@ -3,9 +3,27 @@ include('dbConn.php');
 
 if (isset($_POST['action'])) {
 	if ($_POST['action']=="signup") {
-		$username = mysqli_real_escape_string($connection,$_POST['name']);
-        $email = mysqli_real_escape_string($connection,$_POST['email']);
-        $password = mysqli_real_escape_string($connection,$_POST['password']);
+
+		//Get values from post
+		$username = mysqli_real_escape_string($conn, $_POST['username']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+        //SQL
+        $query = "SELECT email FROM users WHERE email = '".$email."'";
+        $result = mysqli_query($conn, $query); //result of the query
+        $numResults = mysqli_num_rows($result); //num row query returned
+
+        // Validate email address
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+            $message =  "Invalid email address please type a valid email!!";
+        } elseif ($numResults>=1) {
+            $message = $email." Email already exist!!";
+        } else {
+        	echo "query";
+            mysqli_query($conn, "insert into users(username, email, password) values('".$username."','".$email."','".md5($password)."')");
+            $message = "Signup Sucessfully!!";
+        }
 	}
 }
 
--- PHP/dbConn.php
+++ PHP/dbConn.php
@@ -2,13 +2,18 @@
 $servername = "amaceing.db.11908153.hostedresource.com";
 $username = "amaceing";
 $password = "Mandi2005!!!!";
+$dbName = "amaceing";
+$port = 3306;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbName, 3306);
 
 // Check connection
 if ($conn->connect_error) {
+	echo "nope";
     die("Connection failed: " . $conn->connect_error);
+} else {
+	echo "Connected successfully bitch";
 }
-echo "Connected successfully";
+
 ?> 
