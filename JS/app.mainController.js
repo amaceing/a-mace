a8f14075b0367b2a1d6180404dcd2060b25dc489
Anthony Mace <amace93@gmail.com>
2016-07-19 21:45:57 -0700

    very simple sidenav

--- JS/app.mainController.js
+++ JS/app.mainController.js
@@ -1,5 +1,16 @@
-angular.module('portfolioApp')
-.controller('MainController', function ($scope) {
-	$scope.title = "Title";
-	$scope.buttons = ['About', 'Projects', 'Contact']
+angular.module('PortfolioApp', ['ngMaterial']).controller('MainController', 
+
+function($scope) {
+    $scope.title = "My Name Here";
+    $scope.buttons = 
+    [
+        {title:'About', subtext:'Learn more about me'},
+        {title:'Projects', subtext:'See what I\'m working on'},
+        {title:'Contact', subtext:'Get in touch'}
+    ];
+
+
+    $scope.actionButton = function() {
+        console.log("test");
+    }
 });
